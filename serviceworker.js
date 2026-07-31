0a2785b3524612800853ca51e4ca97a1dd479fde
antoneli1982 <antoneli.engenheiro@gmail.com>
2026-07-31 12:22:06 -0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "gestaoepi20260731";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon32.png",
+  "./icons/icon192.png",
+  "./icons/icon512.png",
+  "./icons/apple180.png",
+  "./icons/maskable512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET" || !event.request.url.startsWith(self.location.origin)) return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
